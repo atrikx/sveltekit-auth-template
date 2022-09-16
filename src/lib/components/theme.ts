74f26dc5ef22d595f55215c97ca7d821b5fae501
atrikx <ickynho7@outlook.com>
2022-09-15 21:48:37 -0300

    refactor: :art: Code view improvements

--- src/lib/components/theme.ts
+++ src/lib/components/theme.ts
@@ -1,10 +1,14 @@
+/**
+ * This file contains all colors from components.
+ */
 
 // Button
-export const themeBgButton = '#1D3153';
+export const themeButtonInnerTextColor = '#f6f7f8';
 export const themeButtonBoxShadow = '#132b50';
 export const themeBgButtonHover = '#4D4D4D';
-export const themeButtonInnerTextColor = '#f6f7f8';
+export const themeBgButton = '#1D3153';
 
+// Form and other components coming soon...
 
 
 
--- src/lib/configs.ts
+++ src/lib/configs.ts
@@ -1,3 +1,15 @@
+// Svelte forms
+export const formObjectSignUp: InputText[] = [
+    {name: 'username', placeholder: 'Username', type: "text"},
+    {name: 'email', placeholder: 'Email', type: "text"},
+    {name: 'password', placeholder: 'Password', type: "password"}
+]
+export const formObjectSignIn: InputText[] = [
+    {name: 'email', placeholder: 'Email', type: "text"},
+    {name: 'password', placeholder: 'Password', type: "password"}
+]
+export const buttonsWidth = "50%"
+export const inputsWidth = "60%"
 
 
 // Svelte endpoints
@@ -5,23 +17,10 @@ export const urlMyAccount = '/account'
 export const urlSignUp = '/signup'
 export const urlSignIn = '/signin'
 
-// Svelte forms
-export const inputsWidth = "60%"
-export const buttonsWidth = "50%"
-
-export const formObjectSignIn: InputText[] = [
-    {name: 'email', placeholder: 'Email', type: "text"},
-    {name: 'password', placeholder: 'Password', type: "password"},
-]
-export const formObjectSignUp: InputText[] = [
-    {name: 'username', placeholder: 'Username', type: "text"},
-    {name: 'email', placeholder: 'Email', type: "text"},
-    {name: 'password', placeholder: 'Password', type: "password"},
-]
 
 // API endpoints
 export const apiUrl = 'http://127.0.0.1:8000'
 export const apiNewAccount = apiUrl + '/criar-conta'
+export const apiMyAccount = apiUrl + '/minha-conta' 
 export const apiLogin = apiUrl + '/login'
-export const apiMyAccount = apiUrl + '/minha-conta'
 
